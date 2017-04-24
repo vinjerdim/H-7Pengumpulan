e7e1adb0e53bf9a83f5669b88c5aecb838a6562d
vinjerdim <marjer17dim@gmail.com>
2017-04-25 00:49:10 +0700

    update structure

--- PuzzleSolver/test/playercontrollertest/ComputerPlayerTest.java
+++ PuzzleSolver/test/playercontrollertest/ComputerPlayerTest.java
@@ -1,21 +1,24 @@
 package playercontrollertest;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import model.Puzzle;
+import model.PuzzleSolver;
 
 import org.junit.Test;
 
 import playercontroller.ComputerPlayer;
-import model.Puzzle;
-import model.PuzzleSolver;
+
 
 public class ComputerPlayerTest {
 
-	@Test
-	public void test() {
-		byte size = (byte) 5;
-		Puzzle P = new Puzzle(size);
-		ComputerPlayer CP = new ComputerPlayer(P);
-		assertTrue(CP.getMovementIndex() == 0);
-		assertEquals(CP.getSolution(), (new PuzzleSolver(P)).getSolution().getMovementSet());		
-	}
+  @Test
+  public void test() {
+    byte size = (byte) 5;
+    Puzzle p = new Puzzle(size);
+    ComputerPlayer cp = new ComputerPlayer(p);
+    assertTrue(cp.getMovementIndex() == 0);
+    assertEquals(cp.getSolution(), (new PuzzleSolver(p)).getSolution().getMovementSet());
+  }
 }
